e42695cef68d14c63baabb078446d43cdb759984
Donald Guy <donaldguy@hey.com>
2021-06-24 17:37:34 -0400

    Remove redundant/double-nest mod decleration; fmt

--- src/grid.rs
+++ src/grid.rs
@@ -1,169 +1,247 @@
-mod grid {
-    #[derive(Debug, PartialEq, Eq, Hash)]
-    struct Cube<T: Eq> {
-        value: T,
-        row: usize,
-        col: usize,
-    }
 
-    #[derive(Debug)]
-    struct Grid<T: Eq> {
-        columns: usize,
-        rows: usize,
-        cubes: Vec<Cube<T>>
-    }
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Cube<T: Eq> {
+    value: T,
+    row: usize,
+    col: usize,
+}
 
-    impl<T: Eq> Grid<T> {
-        fn new<I, Tt>(rows: usize, columns: usize, input: I) -> Self
-            where I: IntoIterator<Item=Tt>,
-                  Tt: ToOwned<Owned=T>,
-        {
-            let mut value_it = input.into_iter();
-            let (min_values, _) = value_it.size_hint();
-            if min_values < rows * columns {
-                panic!("Attempted to construct Grid with insufficient sized input. Need {} (rows) * {} (columns) = {}, only offered {}", rows, columns, rows * columns, min_values);
-            }
+#[derive(Debug)]
+struct Grid<T: Eq> {
+    columns: usize,
+    rows: usize,
+    cubes: Vec<Cube<T>>,
+}
 
-            let mut grid: Grid<T> = Self {
-                rows,
-                columns,
-                cubes: Vec::with_capacity(rows * columns)
-            };
-
-            for row in 0..rows {
-                for col in 0..columns {
-                    let value = value_it.next().unwrap();
-                    grid.cubes.push(Cube::<T> {
-                        row,
-                        col,
-                        value: value.to_owned(),
-                    });
-                }
-            }
-
-            grid
+impl<T: Eq> Grid<T> {
+    fn new<I, Tt>(rows: usize, columns: usize, input: I) -> Self
+    where
+        I: IntoIterator<Item = Tt>,
+        Tt: ToOwned<Owned = T>,
+    {
+        let mut value_it = input.into_iter();
+        let (min_values, _) = value_it.size_hint();
+        if min_values < rows * columns {
+            panic!("Attempted to construct Grid with insufficient sized input. Need {} (rows) * {} (columns) = {}, only offered {}", rows, columns, rows * columns, min_values);
         }
 
-        /// get a cube by canonical zero-indexed row and column.
-        ///
-        /// though cubes in memory are linearized, this will return the cube only if referred to by correct index
-        /// (e.g. for `n` column grid: `row: 1, col: 0` ; not `row: 0, col: n`)
-        /// otherwise `None`. This simplifies the implementation of `get_adjacent_to`
-        fn get(&self, row: isize, col: isize) -> Option<&Cube<T>> {
-            if row < 0 || col < 0 {
-                return None
-            }
-            let row = row as usize;
-            let col = col as usize;
+        let mut grid: Grid<T> = Self {
+            rows,
+            columns,
+            cubes: Vec::with_capacity(rows * columns),
+        };
 
-            match self.cubes.get(row * self.columns + col) {
-                None => None, // looking before or after the grid
-                Some(candidate) => {
-                    if candidate.row == row && candidate.col == col {
-                        Some(candidate)
-                    } else { // referred to incoherently with a non-extant column or row
-                        None
-                    }
-                }
+        for row in 0..rows {
+            for col in 0..columns {
+                let value = value_it.next().unwrap();
+                grid.cubes.push(Cube::<T> {
+                    row,
+                    col,
+                    value: value.to_owned(),
+                });
             }
         }
 
-        /// returns iterable of any adjacent cubes that exist
-        // XXX: I'd prefer this be `cube.get_adjacent` or at least `get_adjacent_to(&self, cube: Cube)`
-        // but that slightly better API was a rabbit hole of borrow checking fighting
-        fn get_adjacent_to(&self, row: isize, col: isize) -> impl IntoIterator<Item=&Cube<T>> {
-            //clockwise from "12"
+        grid
+    }
 
-            let up = self.get(row - 1, col + 0);
-            let up_right = self.get(row - 1, col + 1);
-            let right = self.get(row + 0, col + 1);
-            let down_right = self.get(row + 1, col + 1);
-            let down = self.get(row + 1, col + 0);
-            let down_left = self.get(row + 1, col - 1);
-            let left = self.get(row + 0, col - 1);
-            let up_left = self.get(row - 1, col - 1);
+    /// get a cube by canonical zero-indexed row and column.
+    ///
+    /// though cubes in memory are linearized, this will return the cube only if referred to by correct index
+    /// (e.g. for `n` column grid: `row: 1, col: 0` ; not `row: 0, col: n`)
+    /// otherwise `None`. This simplifies the implementation of `get_adjacent_to`
+    fn get(&self, row: isize, col: isize) -> Option<&Cube<T>> {
+        if row < 0 || col < 0 {
+            return None;
+        }
+        let row = row as usize;
+        let col = col as usize;
 
-            let mut result = Vec::with_capacity(5); //OVER-OPTIMIZATION: a 4x4 grid averages 5 neighbors, I believe
-            for maybe_neighbor in &[up, up_right, right, down_right, down, down_left, left, up_left] {
-                if let Some(neighbor) = maybe_neighbor {
-                    result.push(*neighbor)
+        match self.cubes.get(row * self.columns + col) {
+            None => None, // looking before or after the grid
+            Some(candidate) => {
+                if candidate.row == row && candidate.col == col {
+                    Some(candidate)
+                } else {
+                    // referred to incoherently with a non-extant column or row
+                    None
                 }
             }
-
-            result
         }
     }
 
+    /// returns iterable of any adjacent cubes that exist
+    // XXX: I'd prefer this be `cube.get_adjacent` or at least `get_adjacent_to(&self, cube: Cube)`
+    // but that slightly better API was a rabbit hole of borrow checking fighting
+    fn get_adjacent_to(&self, row: isize, col: isize) -> impl IntoIterator<Item = &Cube<T>> {
+        //clockwise from "12"
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        use std::collections::HashSet;
+        let up = self.get(row - 1, col + 0);
+        let up_right = self.get(row - 1, col + 1);
+        let right = self.get(row + 0, col + 1);
+        let down_right = self.get(row + 1, col + 1);
+        let down = self.get(row + 1, col + 0);
+        let down_left = self.get(row + 1, col - 1);
+        let left = self.get(row + 0, col - 1);
+        let up_left = self.get(row - 1, col - 1);
 
-        #[test]
-        fn grid_can_be_built() {
-            let g: Grid<char> = Grid::new(1, 2, vec!['a', 'b']);
-            assert_eq!(g.rows, 1);
-            assert_eq!(g.columns, 2);
-            assert_eq!(g.get(0, 0).unwrap().value, 'a');
-            assert_eq!(g.get(0, 1).unwrap().value, 'b');
+        let mut result = Vec::with_capacity(5); //OVER-OPTIMIZATION: a 4x4 grid averages 5 neighbors, I believe
+        for maybe_neighbor in &[
+            up, up_right, right, down_right, down, down_left, left, up_left,
+        ] {
+            if let Some(neighbor) = maybe_neighbor {
+                result.push(*neighbor)
+            }
         }
 
-        #[test]
-        fn cubes_can_only_be_got_correctly_indexed() {
-            let g = Grid::new(2, 2, vec!['a', 'b', 'c', 'd']);
-            assert_eq!(g.get(0, 0).unwrap().value, 'a');
-            assert_eq!(g.get(0, 1).unwrap().value, 'b');
-            assert_eq!(g.get(1, 0).unwrap().value, 'c');
-            assert_eq!(g.get(1, 1).unwrap().value, 'd');
+        result
+    }
+}
 
-            // this would be 'c' if we weren't checking carefully
-            assert!(g.get(0, 2).is_none());
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
 
-            // whereas these would always be none since they're off the end
-            assert!(g.get(1, 2).is_none());
-            assert!(g.get(2, 0).is_none());
-        }
+    #[test]
+    fn grid_can_be_built() {
+        let g: Grid<char> = Grid::new(1, 2, vec!['a', 'b']);
+        assert_eq!(g.rows, 1);
+        assert_eq!(g.columns, 2);
+        assert_eq!(g.get(0, 0).unwrap().value, 'a');
+        assert_eq!(g.get(0, 1).unwrap().value, 'b');
+    }
 
-        #[test]
-        fn get_adjacent() {
-            // a | b | c | d
-            // e | f | g | h
-            // i | j | k | l
-            // m | n | o | p
+    #[test]
+    fn cubes_can_only_be_got_correctly_indexed() {
+        let g = Grid::new(2, 2, vec!['a', 'b', 'c', 'd']);
+        assert_eq!(g.get(0, 0).unwrap().value, 'a');
+        assert_eq!(g.get(0, 1).unwrap().value, 'b');
+        assert_eq!(g.get(1, 0).unwrap().value, 'c');
+        assert_eq!(g.get(1, 1).unwrap().value, 'd');
 
-            let g: Grid<char> = Grid::new(4, 4, 'a'..='p');
+        // this would be 'c' if we weren't checking carefully
+        assert!(g.get(0, 2).is_none());
 
-            let by_top_corner: HashSet<&Cube<char>> = g.get_adjacent_to(0, 0).into_iter().collect();
-            assert!(by_top_corner.contains(&Cube { row: 0, col: 1, value: 'b' }));
-            assert!(by_top_corner.contains(&Cube { row: 1, col: 0, value: 'e' }));
-            assert!(by_top_corner.contains(&Cube { row: 1, col: 1, value: 'f' }));
-            assert_eq!(by_top_corner.len(), 3);
+        // whereas these would always be none since they're off the end
+        assert!(g.get(1, 2).is_none());
+        assert!(g.get(2, 0).is_none());
+    }
 
-            let by_bottom_corner: HashSet<&Cube<char>> = g.get_adjacent_to(3, 3).into_iter().collect();
-            assert!(by_bottom_corner.contains(&Cube { row: 3, col: 2, value: 'o' }));
-            assert!(by_bottom_corner.contains(&Cube { row: 2, col: 2, value: 'k' }));
-            assert!(by_bottom_corner.contains(&Cube { row: 2, col: 3, value: 'l' }));
-            assert_eq!(by_bottom_corner.len(), 3);
+    #[test]
+    fn get_adjacent() {
+        // a | b | c | d
+        // e | f | g | h
+        // i | j | k | l
+        // m | n | o | p
 
-            let by_h: HashSet<&Cube<char>> = g.get_adjacent_to(1, 3).into_iter().collect();
-            assert!(by_h.contains(&Cube { row: 0, col: 2, value: 'c' }));
-            assert!(by_h.contains(&Cube { row: 0, col: 3, value: 'd' }));
-            assert!(by_h.contains(&Cube { row: 1, col: 2, value: 'g' }));
-            assert!(by_h.contains(&Cube { row: 2, col: 2, value: 'k' }));
-            assert!(by_h.contains(&Cube { row: 2, col: 3, value: 'l' }));
-            assert_eq!(by_h.len(), 5);
+        let g: Grid<char> = Grid::new(4, 4, 'a'..='p');
 
-            let by_f: HashSet<&Cube<char>> = g.get_adjacent_to(1, 1).into_iter().collect();
-            assert!(by_f.contains(&Cube { row: 0, col: 0, value: 'a' }));
-            assert!(by_f.contains(&Cube { row: 0, col: 1, value: 'b' }));
-            assert!(by_f.contains(&Cube { row: 0, col: 2, value: 'c' }));
-            assert!(by_f.contains(&Cube { row: 1, col: 0, value: 'e' }));
-            assert!(by_f.contains(&Cube { row: 1, col: 2, value: 'g' }));
-            assert!(by_f.contains(&Cube { row: 2, col: 0, value: 'i' }));
-            assert!(by_f.contains(&Cube { row: 2, col: 1, value: 'j' }));
-            assert!(by_f.contains(&Cube { row: 2, col: 2, value: 'k' }));
-            assert_eq!(by_f.len(), 8);
-        }
+        let by_top_corner: HashSet<&Cube<char>> = g.get_adjacent_to(0, 0).into_iter().collect();
+        assert!(by_top_corner.contains(&Cube {
+            row: 0,
+            col: 1,
+            value: 'b'
+        }));
+        assert!(by_top_corner.contains(&Cube {
+            row: 1,
+            col: 0,
+            value: 'e'
+        }));
+        assert!(by_top_corner.contains(&Cube {
+            row: 1,
+            col: 1,
+            value: 'f'
+        }));
+        assert_eq!(by_top_corner.len(), 3);
+
+        let by_bottom_corner: HashSet<&Cube<char>> = g.get_adjacent_to(3, 3).into_iter().collect();
+        assert!(by_bottom_corner.contains(&Cube {
+            row: 3,
+            col: 2,
+            value: 'o'
+        }));
+        assert!(by_bottom_corner.contains(&Cube {
+            row: 2,
+            col: 2,
+            value: 'k'
+        }));
+        assert!(by_bottom_corner.contains(&Cube {
+            row: 2,
+            col: 3,
+            value: 'l'
+        }));
+        assert_eq!(by_bottom_corner.len(), 3);
+
+        let by_h: HashSet<&Cube<char>> = g.get_adjacent_to(1, 3).into_iter().collect();
+        assert!(by_h.contains(&Cube {
+            row: 0,
+            col: 2,
+            value: 'c'
+        }));
+        assert!(by_h.contains(&Cube {
+            row: 0,
+            col: 3,
+            value: 'd'
+        }));
+        assert!(by_h.contains(&Cube {
+            row: 1,
+            col: 2,
+            value: 'g'
+        }));
+        assert!(by_h.contains(&Cube {
+            row: 2,
+            col: 2,
+            value: 'k'
+        }));
+        assert!(by_h.contains(&Cube {
+            row: 2,
+            col: 3,
+            value: 'l'
+        }));
+        assert_eq!(by_h.len(), 5);
+
+        let by_f: HashSet<&Cube<char>> = g.get_adjacent_to(1, 1).into_iter().collect();
+        assert!(by_f.contains(&Cube {
+            row: 0,
+            col: 0,
+            value: 'a'
+        }));
+        assert!(by_f.contains(&Cube {
+            row: 0,
+            col: 1,
+            value: 'b'
+        }));
+        assert!(by_f.contains(&Cube {
+            row: 0,
+            col: 2,
+            value: 'c'
+        }));
+        assert!(by_f.contains(&Cube {
+            row: 1,
+            col: 0,
+            value: 'e'
+        }));
+        assert!(by_f.contains(&Cube {
+            row: 1,
+            col: 2,
+            value: 'g'
+        }));
+        assert!(by_f.contains(&Cube {
+            row: 2,
+            col: 0,
+            value: 'i'
+        }));
+        assert!(by_f.contains(&Cube {
+            row: 2,
+            col: 1,
+            value: 'j'
+        }));
+        assert!(by_f.contains(&Cube {
+            row: 2,
+            col: 2,
+            value: 'k'
+        }));
+        assert_eq!(by_f.len(), 8);
     }
 }
