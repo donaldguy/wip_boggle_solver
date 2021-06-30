ace32779499ad7ac75348a0fe744941088b46f25
Donald Guy <donaldguy@hey.com>
2021-06-29 21:57:57 -0400

    get_adjacent_to(row: usize, col: usize) -> get_adjacent_to(cell: &GridCell<T>)
    and get_adjacent_to_in(cell, allowed_cells)
    
    Timesheet reflects a lot of borrow checker fighting that followed from
    an attempt to (prematurely: optimize `get_adjacent_to` by having the
    struct offer a prefab readonly HashSet<&GridCell<T>> w/ all cells to avoid
    reconstructing on each call (which ... the compiler might do anyway?).

--- grid_search/src/grid.rs
+++ grid_search/src/grid.rs
@@ -1,22 +1,23 @@
 use std::collections::HashSet;
+use std::fmt::Debug;
 
 use std::hash::Hash;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
-pub struct GridCell<T: Eq + Hash> {
+pub struct GridCell<T: Eq + Hash + Debug> {
     pub value: T,
     pub row: usize,
     pub col: usize,
 }
 
 #[derive(Debug)]
-pub struct Grid<T: Eq + Hash> {
+pub struct Grid<T: Eq + Hash + Debug> {
     columns: usize,
     rows: usize,
     cells: Vec<GridCell<T>>,
 }
 
-impl<T: Eq + Hash> Grid<T> {
+impl<T: Eq + Hash + Debug> Grid<T> {
     pub fn new<I: IntoIterator<Item = T>>(rows: usize, columns: usize, input: I) -> Self {
         let mut value_it = input.into_iter();
         let max_values = value_it.size_hint().1.unwrap();
@@ -69,14 +70,27 @@ impl<T: Eq + Hash> Grid<T> {
         }
     }
 
-    /// returns iterable of any adjacent cells that exist
-    // XXX: I'd prefer this be `cell.get_adjacent` or at least `get_adjacent_to(&self, cell: GridCell)`
-    // but that slightly better API was a rabbit hole of borrow checking fighting
-    pub fn get_adjacent_to(&self, row: usize, col: usize) -> impl IntoIterator<Item = &GridCell<T>> {
-        //clockwise from "12"
+    pub fn get_adjacent_to(&self, cell: &GridCell<T>) -> impl IntoIterator<Item = &GridCell<T>> {
+        self.get_adjacent_to_in(cell, self.cells_set())
+    }
 
-        let row = row as isize;
-        let col = col as isize;
+    /// returns iterable of any adjacent cells that exist within an allowed set (generally as-yet-unused)
+    pub fn get_adjacent_to_in(
+        &self,
+        cell: &GridCell<T>,
+        allowed_cells: HashSet<&GridCell<T>>,
+    ) -> impl IntoIterator<Item = &GridCell<T>> {
+        let row = cell.row as isize;
+        let col = cell.col as isize;
+
+        if self.get(row, col).unwrap() != cell {
+            panic!(
+                "Attempted to get adjacent cells for cell absent from grid: {:?}",
+                cell
+            )
+        }
+
+        //clockwise from "12"
 
         let up = self.get(row - 1, col);
         let up_right = self.get(row - 1, col + 1);
@@ -92,7 +106,9 @@ impl<T: Eq + Hash> Grid<T> {
             up, up_right, right, down_right, down, down_left, left, up_left,
         ] {
             if let Some(neighbor) = maybe_neighbor {
-                result.push(*neighbor)
+                if allowed_cells.contains(neighbor) {
+                    result.push(*neighbor)
+                }
             }
         }
 
@@ -132,118 +148,67 @@ mod tests {
 
     #[test]
     fn get_adjacent() {
+        let grid: Grid<char> = Grid::new(4, 4, 'a'..='p');
+
         // a | b | c | d
         // e | f | g | h
         // i | j | k | l
         // m | n | o | p
+        let a = grid.get(0, 0).unwrap();
+        let b = grid.get(0, 1).unwrap();
+        let c = grid.get(0, 2).unwrap();
+        let d = grid.get(0, 3).unwrap();
+        let e = grid.get(1, 0).unwrap();
+        let f = grid.get(1, 1).unwrap();
+        let g = grid.get(1, 2).unwrap();
+        let h = grid.get(1, 3).unwrap();
+        let i = grid.get(2, 0).unwrap();
+        let j = grid.get(2, 1).unwrap();
+        let k = grid.get(2, 2).unwrap();
+        let l = grid.get(2, 3).unwrap();
+        let m = grid.get(3, 0).unwrap();
+        let n = grid.get(3, 1).unwrap();
+        let o = grid.get(3, 2).unwrap();
+        let p = grid.get(3, 3).unwrap();
 
-        let g: Grid<char> = Grid::new(4, 4, 'a'..='p');
-
-        let by_top_corner: HashSet<&GridCell<char>> = g.get_adjacent_to(0, 0).into_iter().collect();
-        assert!(by_top_corner.contains(&GridCell {
-            row: 0,
-            col: 1,
-            value: 'b'
-        }));
-        assert!(by_top_corner.contains(&GridCell {
-            row: 1,
-            col: 0,
-            value: 'e'
-        }));
-        assert!(by_top_corner.contains(&GridCell {
-            row: 1,
-            col: 1,
-            value: 'f'
-        }));
+        let by_top_corner: HashSet<&GridCell<char>> = grid.get_adjacent_to(a).into_iter().collect();
+        assert!(by_top_corner.contains(b));
+        assert!(by_top_corner.contains(e));
+        assert!(by_top_corner.contains(f));
         assert_eq!(by_top_corner.len(), 3);
 
-        let by_bottom_corner: HashSet<&GridCell<char>> = g.get_adjacent_to(3, 3).into_iter().collect();
-        assert!(by_bottom_corner.contains(&GridCell {
-            row: 3,
-            col: 2,
-            value: 'o'
-        }));
-        assert!(by_bottom_corner.contains(&GridCell {
-            row: 2,
-            col: 2,
-            value: 'k'
-        }));
-        assert!(by_bottom_corner.contains(&GridCell {
-            row: 2,
-            col: 3,
-            value: 'l'
-        }));
+        let by_bottom_corner: HashSet<&GridCell<char>> =
+            grid.get_adjacent_to(p).into_iter().collect();
+        assert!(by_bottom_corner.contains(o));
+        assert!(by_bottom_corner.contains(k));
+        assert!(by_bottom_corner.contains(l));
         assert_eq!(by_bottom_corner.len(), 3);
 
-        let by_h: HashSet<&GridCell<char>> = g.get_adjacent_to(1, 3).into_iter().collect();
-        assert!(by_h.contains(&GridCell {
-            row: 0,
-            col: 2,
-            value: 'c'
-        }));
-        assert!(by_h.contains(&GridCell {
-            row: 0,
-            col: 3,
-            value: 'd'
-        }));
-        assert!(by_h.contains(&GridCell {
-            row: 1,
-            col: 2,
-            value: 'g'
-        }));
-        assert!(by_h.contains(&GridCell {
-            row: 2,
-            col: 2,
-            value: 'k'
-        }));
-        assert!(by_h.contains(&GridCell {
-            row: 2,
-            col: 3,
-            value: 'l'
-        }));
+        let by_i: HashSet<&GridCell<char>> = grid.get_adjacent_to(i).into_iter().collect();
+        assert!(by_i.contains(e));
+        assert!(by_i.contains(f));
+        assert!(by_i.contains(j));
+        assert!(by_i.contains(n));
+        assert!(by_i.contains(m));
+        assert_eq!(by_i.len(), 5);
+
+        let by_h: HashSet<&GridCell<char>> = grid.get_adjacent_to(h).into_iter().collect();
+        assert!(by_h.contains(c));
+        assert!(by_h.contains(d));
+        assert!(by_h.contains(g));
+        assert!(by_h.contains(k));
+        assert!(by_h.contains(l));
         assert_eq!(by_h.len(), 5);
 
-        let by_f: HashSet<&GridCell<char>> = g.get_adjacent_to(1, 1).into_iter().collect();
-        assert!(by_f.contains(&GridCell {
-            row: 0,
-            col: 0,
-            value: 'a'
-        }));
-        assert!(by_f.contains(&GridCell {
-            row: 0,
-            col: 1,
-            value: 'b'
-        }));
-        assert!(by_f.contains(&GridCell {
-            row: 0,
-            col: 2,
-            value: 'c'
-        }));
-        assert!(by_f.contains(&GridCell {
-            row: 1,
-            col: 0,
-            value: 'e'
-        }));
-        assert!(by_f.contains(&GridCell {
-            row: 1,
-            col: 2,
-            value: 'g'
-        }));
-        assert!(by_f.contains(&GridCell {
-            row: 2,
-            col: 0,
-            value: 'i'
-        }));
-        assert!(by_f.contains(&GridCell {
-            row: 2,
-            col: 1,
-            value: 'j'
-        }));
-        assert!(by_f.contains(&GridCell {
-            row: 2,
-            col: 2,
-            value: 'k'
-        }));
+        let by_f: HashSet<&GridCell<char>> = grid.get_adjacent_to(f).into_iter().collect();
+        assert!(by_f.contains(a));
+        assert!(by_f.contains(b));
+        assert!(by_f.contains(c));
+        assert!(by_f.contains(e));
+        assert!(by_f.contains(g));
+        assert!(by_f.contains(i));
+        assert!(by_f.contains(j));
+        assert!(by_f.contains(k));
         assert_eq!(by_f.len(), 8);
     }
 }
